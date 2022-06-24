1a856b711f646fd5a4c32aedd2af664f1c710c9c
Esmatullah Ahmadzai <ea.ahmadzai2020@gmail.com>
2022-06-24 08:47:50 +0430

    Add file to used to collect data of form

---
+++ script.js
@@ -0,0 +1,30 @@
+
+// Form input to localStorage
+
+// function getInTouch(e)
+form.addEventListener('submit', () => {
+  const formInput = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    textmessage: document.getElementById('message').value,
+  };
+  localStorage.setItem('formInput', JSON.stringify(formInput));
+});
+
+function inputDisplay() {
+  if (localStorage.getItem('formInput')) {
+    const {
+      name,
+      email,
+      textmessage,
+    } = JSON.parse(localStorage.getItem('formInput'));
+    const outputName = document.getElementById('name');
+    const outputEmail = document.getElementById('email');
+    const outPutTextmessage = document.getElementById('message');
+    outputName.value = name;
+    outputEmail.value = email;
+    outPutTextmessage.value = textmessage;
+  }
+}
+
+inputDisplay();
